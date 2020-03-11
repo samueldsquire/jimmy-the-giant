57e41e188f91c02decfc6083c5f3c0062026e72b
Sam Squire <sam@expandia.co>
2020-03-11 19:14:11 +0000

    Initial upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($){
+	$('#sideMenuOpener').on('click', function(){
+		$('#sideMenu').toggleClass('sideMenuOpen');
+		$('.entire-site').toggleClass('entire-site--hidden');
+		$('.menu-icon').toggleClass('transformed');
+		$('.menu-icon__container').toggleClass('transformed');
+	});
+	$('#sideMenu').on('click', function(){
+		$('#sideMenu').removeClass('sideMenuOpen');
+		$('.entire-site').removeClass('entire-site--hidden');
+		$('.menu-icon').removeClass('transformed');
+		$('.menu-icon__container').removeClass('transformed');
+	});
+	$(document).keyup(function(e) { 
+	    if (e.keyCode == 27) {
+	    	$('#sideMenu').removeClass('sideMenuOpen');
+	    	$('.entire-site').removeClass('entire-site--hidden');
+	    	$('.menu-icon').removeClass('transformed');
+	    	$('.menu-icon__container').removeClass('transformed');
+	    } 
+	});
+});
